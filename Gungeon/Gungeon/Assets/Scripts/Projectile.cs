3edbe03a9b0c8edaca3185c7d6a58acdbaa4ead4
wslatts <wslatts12@gmail.com>
2019-10-24 21:44:33 -0400

    projectileFunction_wendy
    projectile throws with trajectory from firepoint to target upon gameplay...w/bugs

---
+++ Gungeon/Gungeon/Assets/Scripts/Projectile.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class Projectile : MonoBehaviour
+{
+    public Transform target;
+    public Transform firePoint;  // the position from where object is thrown 
+    public GameObject bullet;    // the object to throw
+    public float strikeTiming = 1f;
+
+
+    void Start()
+    {
+        Throw();
+    }
+
+    void Throw()
+    {
+
+        float xDistance;
+        xDistance = target.position.x - firePoint.position.x;
+
+        float yDistance;
+        yDistance = target.position.y - firePoint.position.y;
+
+        float fireAngle; // in radians
+        fireAngle = Mathf.Atan((yDistance + 4.905f * (strikeTiming * strikeTiming)) / xDistance); //using math from Jesse Mason's video toolbox method
+        float totalVelocity = xDistance / (Mathf.Cos(fireAngle) * strikeTiming);
+
+        float xVelocity, yVelocity;
+        xVelocity = totalVelocity * Mathf.Cos(fireAngle);
+        yVelocity = totalVelocity * Mathf.Sin(fireAngle);
+
+        GameObject bulletInstance = Instantiate(bullet, firePoint.position, Quaternion.Euler(new Vector3(0, 0, 0))) as GameObject;
+        Rigidbody2D rigidBody;
+        rigidBody = bulletInstance.GetComponent<Rigidbody2D>();
+
+        rigidBody.velocity = new Vector2(xVelocity, yVelocity);
+    }
+}
+
