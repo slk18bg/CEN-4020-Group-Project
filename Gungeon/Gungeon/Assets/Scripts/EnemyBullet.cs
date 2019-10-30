8df9566b8758cdc179ed709f8b256459267f2b37
wslatts <wslatts12@gmail.com>
2019-10-30 17:06:23 -0400

    enemy_fire_2_wendy
    resolved bug so enemies take damage from player bullets now

--- Gungeon/Gungeon/Assets/Scripts/EnemyBullet.cs
+++ Gungeon/Gungeon/Assets/Scripts/EnemyBullet.cs
@@ -8,7 +8,6 @@ public class EnemyBullet : MonoBehaviour
     public int damage = 40;
     public Rigidbody2D rigidBody;
 
-    // Start is called before the first frame update
     void Start()
     {
         rigidBody.velocity = transform.right * speed;
