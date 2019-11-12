35a3688214de4ef8542aa26da74907bf9b790611
wslatts <wslatts12@gmail.com>
2019-11-11 22:17:44 -0500

    minionShooting_1_wendy
    minion shooting both directions now...adjusted the movement transitions inside GFX script

--- Gungeon/Gungeon/Assets/Scripts/EnemyGFX.cs
+++ Gungeon/Gungeon/Assets/Scripts/EnemyGFX.cs
@@ -5,10 +5,13 @@ using Pathfinding;
 
 public class EnemyGFX : MonoBehaviour
 {
+    public Transform targetPlayer;
+
     public AIPath aiPath;
 
     void Update()
     {
+        /*
         if (aiPath.desiredVelocity.x <= 0.01f) //if x speed is moving right then icon faces right
         {
             transform.localScale = new Vector3(-1f, 1f, 1f);
@@ -16,6 +19,17 @@ public class EnemyGFX : MonoBehaviour
         else if(aiPath.desiredVelocity.x >= -0.01f) //if x speed is moving left, flip enemy facing direction to left
         {
             transform.localScale = new Vector3(1f, 1f, 1f);
-        }        
+        }
+        */
+        // make sure enemy is always facing player
+        if (transform.position.x > targetPlayer.position.x)
+        {
+            transform.rotation = Quaternion.Euler(0, 180, 0);
+        }
+
+        else
+        {
+            transform.rotation = Quaternion.Euler(0, 0, 0);
+        }
     }
 }
