ca0d1fd3331f544541c924a341516752e39ee733
wslatts <wslatts12@gmail.com>
2019-11-12 15:05:37 -0500

    playerShooting_3_wendy
    player gun stationary but bullets shoot varying degrees and toward mouse pointer

--- Gungeon/Gungeon/Assets/Scripts/PlayerWeapon.cs
+++ Gungeon/Gungeon/Assets/Scripts/PlayerWeapon.cs
@@ -17,7 +17,11 @@ public class PlayerWeapon : MonoBehaviour
 
         Vector2 lookDirection = Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position;
         float lookAngle = Mathf.Atan2(lookDirection.y, lookDirection.x) * Mathf.Rad2Deg;
-        transform.rotation = Quaternion.Euler(0f,0f,lookAngle);
+        if(lookAngle <= 80f || lookAngle >= -80f)
+        {
+            transform.rotation = Quaternion.Euler(0f, 0f, lookAngle);
+        }
+        
 
         if (Input.GetButtonDown("Fire1"))
         {
