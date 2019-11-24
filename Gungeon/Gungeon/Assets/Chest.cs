64557a94b0514b6027215f23d1bfcda95c94be0d
lucacs <llucacs1@gmail.com>
2019-11-24 23:18:12 +0100

    ChestImprovedIdleAni

--- Gungeon/Gungeon/Assets/Chest.cs
+++ Gungeon/Gungeon/Assets/Chest.cs
@@ -4,25 +4,33 @@ using UnityEngine;
 
 public class Chest : MonoBehaviour
 {
-    //public Animator chest;
+    public Animator chest;
     public GameObject[] objects;
     public Transform spawnPoint;
     private GameObject item;
     private bool chestOpened;
-    public float Timer = 5;
+    private float Timer = 10;
+    
 
     private void Update()
     {
         Timer -= Time.deltaTime;
+        if (Timer <= 0)
+        {
+            //GetComponent<Animator>().SetTrigger("ItemReady");
+            chest.SetBool("ItemReady", true);
+        }
     }
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.CompareTag ("Player") && Timer <= 0)  //&& ((Timer - Time.deltaTime) <= 0) //&& !chestOpened)
         {
             //chestOpened = true;
-            GetComponent<Animator>().SetBool("open",true);
+            //GetComponent<Animator>().SetBool("open",true);
+            chest.SetBool("open", true);
+            chest.SetBool("ItemReady", false);
             item = Instantiate(objects[Random.Range(0, objects.Length)], spawnPoint.position, spawnPoint.rotation) as GameObject;
-            Timer = 5;
+            Timer = 10;
         }
     }
 
@@ -31,7 +39,8 @@ public class Chest : MonoBehaviour
         if (collision.CompareTag("Player"))
         {
             Destroy(item);
-            GetComponent<Animator>().SetBool("open", false);
+            //GetComponent<Animator>().SetBool("open", false);
+            chest.SetBool("open", false);
         }
     }
 }
