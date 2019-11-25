96c1697b8ff37fc0a2e83f79393b913a7633cde3
lucacs <llucacs1@gmail.com>
2019-11-25 22:45:02 +0100

    AddedSimpleBoxCoin

---
+++ Gungeon/Gungeon/Assets/Scripts/boxItem.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class boxItem : MonoBehaviour
+{
+    //public Transform spawnPoint;
+    public GameObject item;
+    private bool broken;
+
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player") && !broken)
+        {
+            broken = true;
+            GetComponent<Animator>().SetTrigger("break");
+            item.SetActive(true);
+        }
+    }
+}
