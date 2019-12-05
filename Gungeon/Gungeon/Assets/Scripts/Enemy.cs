87ba7b5ab1251acfe4bf994f079049e14522e151
Sheikh Khaled <42755571+slk18bg@users.noreply.github.com>
2019-12-05 15:00:17 -0500

    Game Win Added
    Game win scene added

--- Gungeon/Gungeon/Assets/Scripts/Enemy.cs
+++ Gungeon/Gungeon/Assets/Scripts/Enemy.cs
@@ -3,20 +3,26 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Audio;
 
+
+
 public class Enemy : MonoBehaviour
 {
     public int health = 100;
     public GameObject rewardPrefab;
     public GameObject deathReaction;
     public bool deathEffect = true;
-        
+
+    public GameObject GameWin;
+    public static bool GamePaused = false;
+
     public void TakeDamage(int damage)
     {
         health -= damage;
 
         if (health <= 0)
         {
-            Die();                                   
+            Die();
+            GameWon();
         }
     }
 
@@ -34,5 +40,12 @@ public class Enemy : MonoBehaviour
     private void DropReward()
     {        
         Instantiate(rewardPrefab, transform.position, transform.rotation);
+    }
+
+    public void GameWon()
+    {
+        GameWin.SetActive(true); // enables pause menu
+        Time.timeScale = 0f; // freeze time
+        GamePaused = true;
     }
 }
